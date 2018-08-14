5f6f7c08747e224e5b23394c435cbe559b13f3fe
Norbert <susztek.norbert@gmail.com>
2018-08-14 12:26:20 +0200

    initial

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const session     = require('express-session');
 const mongo       = require('mongodb').MongoClient;
 const passport    = require('passport');
-
+const GitHubStrategy = require('passport-github').Strategy;
+const nodemon = require('nodemon');
+require('dotenv').config();
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
@@ -16,30 +18,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'pug')
+// app.set('views','./views/pug')
 
 mongo.connect(process.env.DATABASE, (err, db) => {
     if(err) {
         console.log('Database error: ' + err);
     } else {
         console.log('Successful database connection');
-      
+
         app.use(session({
-          secret: process.env.SESSION_SECRET,
-          resave: true,
-          saveUninitialized: true,
+            secret: process.env.SESSION_SECRET,
+            resave: true,
+            saveUninitialized: true,
         }));
         app.use(passport.initialize());
         app.use(passport.session());
-      
+
         function ensureAuthenticated(req, res, next) {
-          if (req.isAuthenticated()) {
-              return next();
-          }
-          res.redirect('/');
+            if (req.isAuthenticated()) {
+                return next();
+            }
+            res.redirect('/');
         };
 
         passport.serializeUser((user, done) => {
-          done(null, user.id);
+            done(null, user.id);
         });
 
         passport.deserializeUser((id, done) => {
@@ -51,45 +54,78 @@ mongo.connect(process.env.DATABASE, (err, db) => {
             );
         });
 
-      
+
         /*
         *  ADD YOUR CODE BELOW
         */
-      
-      
-      
-      
-      
-      
-      
+
+
+        passport.use(new GitHubStrategy({
+                clientID: process.env.GITHUB_CLIENT_ID,
+                clientSecret: process.env.GITHUB_CLIENT_SECRET,
+                callbackURL: "https://socialauth-ns.herokuapp.com/auth/github/callback"
+            },
+            function(accessToken, refreshToken, profile, cb) {
+                console.log(profile);
+                db.collection('socialusers').findAndModify(
+                    {id: profile.id},
+                    {},
+                    {$setOnInsert:{
+                            id: profile.id,
+                            name: profile.displayName || 'John Doe',
+                            photo: profile.photos[0].value || '',
+                            // email: profile.email[0].value || 'No public email',
+                            created_on: new Date(),
+                            provider: profile.provider || ''
+                        },$set:{
+                            last_login: new Date()
+                        },$inc:{
+                            login_count: 1
+                        }},
+                    {upsert:true, new: true}, //Insert object if not found, Return new object after modify
+                    (err, doc) => {
+                        return cb(null, doc.value);
+                    }
+                );
+            }
+        ));
+
+        app.route('/auth/github')
+            .get(passport.authenticate('github'));
+
+        app.route('/auth/github/callback')
+            .get(passport.authenticate('github', { failureRedirect: '/' }), (req,res) => {
+                res.redirect('/profile');
+            });
+
         /*
         *  ADD YOUR CODE ABOVE
         */
-      
-      
+
+
         app.route('/')
-          .get((req, res) => {
-            res.render(process.cwd() + '/views/pug/index');
-          });
+            .get((req, res) => {
+                res.render(process.cwd() + '/views/pug/index');
+            });
 
         app.route('/profile')
-          .get(ensureAuthenticated, (req, res) => {
-               res.render(process.cwd() + '/views/pug/profile', {user: req.user});
-          });
+            .get(ensureAuthenticated, (req, res) => {
+                res.render(process.cwd() + '/views/pug/profile', {user: req.user});
+            });
 
         app.route('/logout')
-          .get((req, res) => {
-              req.logout();
-              res.redirect('/');
-          });
+            .get((req, res) => {
+                req.logout();
+                res.redirect('/');
+            });
 
         app.use((req, res, next) => {
-          res.status(404)
-            .type('text')
-            .send('Not Found');
+            res.status(404)
+                .type('text')
+                .send('Not Found');
         });
-      
+
         app.listen(process.env.PORT || 3000, () => {
-          console.log("Listening on port " + process.env.PORT);
-        });  
-}});
+            console.log("Listening on port " + process.env.PORT);
+        });
+    }});
